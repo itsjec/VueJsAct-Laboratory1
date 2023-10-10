5541a720f645f680396d796a87236db1bd724a1a
Kristia08 <128823391+Kristia08@users.noreply.github.com>
2023-10-11 03:01:39 +0800

    /manage

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/IndexView.vue'
+import ManageView from '../views/ManageView.vue'
 import FormsView from '../views/FormsView.vue'
 
 const routes = [
@@ -15,15 +16,12 @@ const routes = [
   },
     
   
-  //add Kristia's path
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/manage',
+    name: 'manage',
+    component: ManageView,
+    },
+    
 ]
 
 const router = createRouter({
---
+++ src/views/ManageView.vue
@@ -0,0 +1,16 @@
+<template>
+    <sidebar />
+    <tables />
+</template>
+<script>
+import { defineComponent } from 'vue';
+import sidebar from '@/components/sidebar.vue';
+import tables from '@/components/tables.vue';
+export default defineComponent({
+  name: 'manage',
+  components: {
+    sidebar,
+    tables,
+  },
+});
+</script>
